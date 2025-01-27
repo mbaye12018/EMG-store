a96619e488fe6405b16a02f0b1bf0a7f269bf2d8
mbayedieng <mbayedieng@esp.sn>
2025-01-27 21:50:33 +0000

    frontEnd progrssing

--- obj/Debug/net8.0/EMGStores.AssemblyInfo.cs
+++ obj/Debug/net8.0/EMGStores.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EMGStores")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc4d9dc6c9cc52c5c49e99c481e75b15bc339eb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("EMGStores")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EMGStores")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
